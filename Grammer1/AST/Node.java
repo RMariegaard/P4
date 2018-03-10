824dc65b5f870d9ce34dccb01e917f53f91b3536
casper-lp <cpede16@student.aau.dk>
2018-03-10 13:07:36 +0100

    glemte at synce i fredags

---
+++ Grammer1/AST/Node.java
@@ -0,0 +1,13 @@
+abstract class Node{
+
+    public Node Parent;
+    public Node LeftmostSibling;
+    public Node RightSibling;
+    public Node LeftmostChild;
+
+    public Node(){}
+
+    public MakeSiblings(){}
+
+    public AdoptChildren(){}
+}
