3ef1a9af70986df6a850934531354372f2d4613c
Jackilans <mark1904@hotmail.dk>
2018-04-13 14:12:21 +0200

    code Generation done

---
+++ antlr/src/CodeGeneratorVisitor.java
@@ -0,0 +1,224 @@
+import Nodes.*;
+import Nodes.expr.*;
+import Nodes.values.*;
+
+public class CodeGeneratorVisitor extends AstVisitor<String> {
+    @Override
+    public String Visit(ProgNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(WhileStmtNode node) {
+        String someOutputPlaceIDK;
+
+        someOutputPlaceIDK = String.format("while(%s){%s}",Visit(node.ConditionNode()), Visit(node.BlockNode()));
+        return someOutputPlaceIDK;
+    }
+
+    @Override
+    public String Visit(UsubNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(UAddNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(StrategyNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(SetupNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(RTypeNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(ReturnStmtNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(RefNode node) {
+        return null;
+    }
+
+
+    @Override
+    public String Visit(OrNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(NullNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(MethodNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(LessNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(LessEqualNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(IfStmtNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(GreaterNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(GreaterEqualNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(GameLoopNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(EventNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(EqualNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(ElseNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(ElseIfNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(DoStmtNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(DclNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(BlockNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(BehaviorNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(AssignNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(ArgumentNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(AndNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(ActionNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(SubExprNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(NotExprNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(MulExprNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(InfixExprNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(DivExprNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(BoolExprNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(ArrayExprNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(AddExprNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(IntNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(IDNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(DecimalNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(StringNode node) {
+        return null;
+    }
+
+    @Override
+    public String Visit(BoolNode node) {
+        return null;
+    }
+}
