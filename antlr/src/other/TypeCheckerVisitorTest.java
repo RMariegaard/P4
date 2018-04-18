8817a33fd3783f3ab80d9823dfbfb721110dcc28
Jackilans <mark1904@hotmail.dk>
2018-04-18 14:32:37 +0200

    test

---
+++ antlr/src/other/TypeCheckerVisitorTest.java
@@ -0,0 +1,180 @@
+package other;
+
+class TypeCheckerVisitorTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit1() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit2() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit3() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit4() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit5() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit6() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit7() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit8() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit9() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit10() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit11() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit12() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit13() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit14() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit15() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit16() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit17() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit18() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit19() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit20() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit21() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit22() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit23() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit24() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit25() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit26() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit27() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit28() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit29() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit30() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit31() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit32() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit33() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit34() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit35() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit36() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit37() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit38() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit39() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit40() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit41() {
+    }
+}
